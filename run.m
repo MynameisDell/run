2403d0c97998c494a5661e7ce26ff369250d7fea
Dell <93853674+MynameisDell@users.noreply.github.com>
2024-12-03 06:05:27 +0700

    Update run.m
    fix run

--- run.m
+++ run.m
@@ -8,23 +8,150 @@
 extern int SBSLaunchApplicationWithIdentifier(CFStringRef identifier, Boolean suspended);
 extern CFStringRef SBSApplicationLaunchingErrorString(int error);
 
-CFStringRef GetBundleIdentifier(CFStringRef appName) {
+typedef enum {
+    SEARCH_EXACT_MATCH,
+    SEARCH_PARTIAL_MATCH,
+    SEARCH_CASE_INSENSITIVE
+} SearchMode;
+
+typedef struct {
+    CFStringRef bundleIdentifier;
+    CFStringRef displayName;
+    CFStringRef path;
+} ApplicationInfo;
+
+CFArrayRef SearchApplications(CFStringRef searchTerm, SearchMode mode) {
+    CFMutableArrayRef matchedApps = CFArrayCreateMutable(NULL, 0, NULL);
+    
     id workspace = ((id (*)(id, SEL))objc_msgSend)(objc_getClass("LSApplicationWorkspace"), sel_registerName("defaultWorkspace"));
     NSArray *apps = ((NSArray *(*)(id, SEL))objc_msgSend)(workspace, sel_registerName("allApplications"));
 
     for (id app in apps) {
         CFStringRef displayName = ((CFStringRef (*)(id, SEL))objc_msgSend)(app, sel_registerName("localizedName"));
-        if (displayName && (CFStringCompare(displayName, appName, 0) == kCFCompareEqualTo || CFStringFind(displayName, appName, kCFCompareAnchored).location != kCFNotFound)) {
-            return ((CFStringRef (*)(id, SEL))objc_msgSend)(app, sel_registerName("bundleIdentifier"));
+        CFStringRef bundleID = ((CFStringRef (*)(id, SEL))objc_msgSend)(app, sel_registerName("bundleIdentifier"));
+        
+        Boolean matchFound = false;
+        
+        switch(mode) {
+            case SEARCH_EXACT_MATCH:
+                matchFound = (CFStringCompare(displayName, searchTerm, 0) == kCFCompareEqualTo);
+                break;
+            
+            case SEARCH_PARTIAL_MATCH:
+                matchFound = (CFStringFind(displayName, searchTerm, kCFCompareAnchored).location != kCFNotFound);
+                break;
+            
+            case SEARCH_CASE_INSENSITIVE:
+                matchFound = (CFStringFindWithOptions(displayName, searchTerm, 
+                                                      CFRangeMake(0, CFStringGetLength(displayName)), 
+                                                      kCFCompareCaseInsensitive, 
+                                                      NULL) != 0);
+                break;
+        }
+        
+        if (matchFound) {
+            ApplicationInfo *appInfo = malloc(sizeof(ApplicationInfo));
+            appInfo->bundleIdentifier = CFRetain(bundleID);
+            appInfo->displayName = CFRetain(displayName);
+            
+            id url = ((id (*)(id, SEL))objc_msgSend)(app, sel_registerName("bundleURL"));
+            CFStringRef path = ((CFStringRef (*)(id, SEL))objc_msgSend)(url, sel_registerName("path"));
+            appInfo->path = CFRetain(path);
+            
+            CFArrayAppendValue(matchedApps, appInfo);
         }
     }
-    return NULL;
+    
+    return matchedApps;
+}
+
+void PrintApplicationResults(CFArrayRef results) {
+    CFIndex count = CFArrayGetCount(results);
+    printf("Found %ld applications:\n", count);
+    
+    for (CFIndex i = 0; i < count; i++) {
+        ApplicationInfo *appInfo = (ApplicationInfo *)CFArrayGetValueAtIndex(results, i);
+        
+        char bundleIDStr[256], displayNameStr[256], pathStr[1024];
+        CFStringGetCString(appInfo->bundleIdentifier, bundleIDStr, sizeof(bundleIDStr), kCFStringEncodingUTF8);
+        CFStringGetCString(appInfo->displayName, displayNameStr, sizeof(displayNameStr), kCFStringEncodingUTF8);
+        CFStringGetCString(appInfo->path, pathStr, sizeof(pathStr), kCFStringEncodingUTF8);
+        
+        printf("%ld. Name: %s\n   Bundle ID: %s\n   Path: %s\n\n",
+               i+1, displayNameStr, bundleIDStr, pathStr);
+    }
+}
+
+void CleanupSearchResults(CFArrayRef results) {
+    CFIndex count = CFArrayGetCount(results);
+    for (CFIndex i = 0; i < count; i++) {
+        ApplicationInfo *appInfo = (ApplicationInfo *)CFArrayGetValueAtIndex(results, i);
+        CFRelease(appInfo->bundleIdentifier);
+        CFRelease(appInfo->displayName);
+        CFRelease(appInfo->path);
+        free(appInfo);
+    }
+    CFRelease(results);
+}
+
+CFStringRef GetBundleIdentifier(CFStringRef searchTerm) {
+    CFArrayRef exactResults = SearchApplications(searchTerm, SEARCH_EXACT_MATCH);
+    CFArrayRef partialResults = SearchApplications(searchTerm, SEARCH_PARTIAL_MATCH);
+    CFArrayRef caseInsensitiveResults = SearchApplications(searchTerm, SEARCH_CASE_INSENSITIVE);
+    
+    CFStringRef bundleID = NULL;
+    if (CFArrayGetCount(exactResults) > 0) {
+        ApplicationInfo *appInfo = (ApplicationInfo *)CFArrayGetValueAtIndex(exactResults, 0);
+        bundleID = CFRetain(appInfo->bundleIdentifier);
+    }
+    else if (CFArrayGetCount(partialResults) > 0) {
+        ApplicationInfo *appInfo = (ApplicationInfo *)CFArrayGetValueAtIndex(partialResults, 0);
+        bundleID = CFRetain(appInfo->bundleIdentifier);
+    }
+    else if (CFArrayGetCount(caseInsensitiveResults) > 0) {
+        ApplicationInfo *appInfo = (ApplicationInfo *)CFArrayGetValueAtIndex(caseInsensitiveResults, 0);
+        bundleID = CFRetain(appInfo->bundleIdentifier);
+    }
+
+    CleanupSearchResults(exactResults);
+    CleanupSearchResults(partialResults);
+    CleanupSearchResults(caseInsensitiveResults);
+    
+    return bundleID;
+}
+
+void ListAllApplications() {
+    id workspace = ((id (*)(id, SEL))objc_msgSend)(objc_getClass("LSApplicationWorkspace"), sel_registerName("defaultWorkspace"));
+    NSArray *apps = ((NSArray *(*)(id, SEL))objc_msgSend)(workspace, sel_registerName("allApplications"));
+
+    printf("Installed applications:\n\n");
+    CFIndex index = 1;
+    for (id app in apps) {
+        CFStringRef displayName = ((CFStringRef (*)(id, SEL))objc_msgSend)(app, sel_registerName("localizedName"));
+        CFStringRef bundleID = ((CFStringRef (*)(id, SEL))objc_msgSend)(app, sel_registerName("bundleIdentifier"));
+        id url = ((id (*)(id, SEL))objc_msgSend)(app, sel_registerName("bundleURL"));
+        CFStringRef path = ((CFStringRef (*)(id, SEL))objc_msgSend)(url, sel_registerName("path"));
+
+        char displayNameStr[256], bundleIDStr[256], pathStr[1024];
+        CFStringGetCString(displayName, displayNameStr, sizeof(displayNameStr), kCFStringEncodingUTF8);
+        CFStringGetCString(bundleID, bundleIDStr, sizeof(bundleIDStr), kCFStringEncodingUTF8);
+        CFStringGetCString(path, pathStr, sizeof(pathStr), kCFStringEncodingUTF8);
+
+        printf("%ld. Name: %s\n   Bundle ID: %s\n   Path: %s\n\n", 
+               index++, displayNameStr, bundleIDStr, pathStr);
+    }
 }
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <com.application.identifier | Application Name>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <application name or bundle identifier> | -l\n", argv[0]);
         return -1;
+    }
+
+    if (strcmp(argv[1], "-l") == 0) {
+        // Option to list all installed applications
+        ListAllApplications();
+        return 0;
     }
 
     char combinedArgs[1024] = {0};
@@ -35,7 +162,7 @@ int main(int argc, char **argv) {
 
     CFStringRef input = CFStringCreateWithCString(NULL, combinedArgs, kCFStringEncodingUTF8);
     if (!input) {
-        fprintf(stderr, "Failed to create CFStringRef\n");
+        fprintf(stderr, "Could not create CFStringRef\n");
         return -1;
     }
 
@@ -49,7 +176,7 @@ int main(int argc, char **argv) {
     }
 
     if (ret != 0) {
-        fprintf(stderr, "Couldn't open application: %s. Reason: %i, ", combinedArgs, ret);
+        fprintf(stderr, "Could not open application: %s. Error code: %i, ", combinedArgs, ret);
         CFShow(SBSApplicationLaunchingErrorString(ret));
     }
 
